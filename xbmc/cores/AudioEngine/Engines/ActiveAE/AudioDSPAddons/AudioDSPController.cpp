2f804058390665dfd84e9059fcb7d32ce98f3d35
AchimTuran <mail@achim-turan.de>
2017-04-01 09:03:17 +0200

    [adsp] initial commit of CAudioDSPController class

---
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/AudioDSPAddons/AudioDSPController.cpp
@@ -0,0 +1,55 @@
+/*
+ *      Copyright (C) 2005-2017 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "AudioDSPController.h"
+
+using namespace ActiveAE;
+using namespace DSP;
+
+
+CAudioDSPController::CAudioDSPController(IDSPNodeModel &DSPNodeModel) :
+  m_DSPNodeModel(DSPNodeModel)
+{
+}
+
+IDSPNodeModel::CDSPNodeInfo CAudioDSPController::GetNodeInfo(IDSPNodeModel::CDSPNodeInfoQuery &Node)
+{
+  return m_DSPNodeModel.GetNodeInfo(Node);
+}
+
+DSPErrorCode_t CAudioDSPController::GetNodeInfos(IDSPNodeModel::DSPNodeInfoVector_t &NodeInfos)
+{
+  return m_DSPNodeModel.GetNodeInfos(NodeInfos);
+}
+
+DSPErrorCode_t CAudioDSPController::GetActiveNodes(IDSPNodeModel::DSPNodeInfoVector_t &ActiveNodeInfos)
+{
+  return m_DSPNodeModel.GetActiveNodes(ActiveNodeInfos);
+}
+
+DSPErrorCode_t CAudioDSPController::DisableNode(uint64_t ID)
+{
+  return m_DSPNodeModel.DisableNode(ID);
+}
+
+DSPErrorCode_t CAudioDSPController::EnableNode(uint64_t ID, uint32_t Position)
+{
+  return m_DSPNodeModel.EnableNode(ID, Position);
+}
---
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/AudioDSPAddons/AudioDSPController.h
@@ -0,0 +1,46 @@
+/*
+ *      Copyright (C) 2005-2017 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include "cores/DSP/Models/Interfaces/IDSPNodeModel.h"
+
+
+namespace ActiveAE
+{
+class CAudioDSPController
+{
+public:
+  CAudioDSPController(DSP::IDSPNodeModel &DSPNodeModel);
+
+  // node infos
+  DSP::IDSPNodeModel::CDSPNodeInfo GetNodeInfo(DSP::IDSPNodeModel::CDSPNodeInfoQuery &Node);
+  DSPErrorCode_t GetNodeInfos(DSP::IDSPNodeModel::DSPNodeInfoVector_t &NodeInfos);
+  DSPErrorCode_t GetActiveNodes(DSP::IDSPNodeModel::DSPNodeInfoVector_t &ActiveNodeInfos);
+
+  // node managing
+  DSPErrorCode_t DisableNode(uint64_t ID);
+  // Position == 0 --> adds node to the end of active nodes
+  DSPErrorCode_t EnableNode(uint64_t ID, uint32_t Position = 0);
+
+private:
+  DSP::IDSPNodeModel &m_DSPNodeModel;
+};
+}
