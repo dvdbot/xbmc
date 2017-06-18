65ab325a043f82d4e7a9c9825003a961fba13c00
Garrett Brown <themagnificentmrb@gmail.com>
2017-06-17 20:27:56 -0700

    Joysticks: Refactor code behind new IActionHandler interface

---
+++ xbmc/input/IActionHandler.h
@@ -0,0 +1,47 @@
+/*
+ *      Copyright (C) 2017 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this Program; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+class CAction;
+
+namespace KODI
+{
+namespace ACTIONS
+{
+
+/*!
+ * \ingroup joystick
+ * \brief Interface for handling Kodi actions
+ */
+class IActionHandler
+{
+public:
+  virtual ~IActionHandler() = default;
+
+  /*!
+   * \brief Send a digital or analog action
+   * \param action The action to send
+   * \return True if the action was handled, false otherwise
+   */
+  virtual bool SendAction(const CAction& action) = 0;
+};
+
+}
+}
