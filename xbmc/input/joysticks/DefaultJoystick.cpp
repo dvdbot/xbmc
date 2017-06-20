77ba23ab2eafcb8ce7a5a8d6e563988d60e11cd3
Garrett Brown <themagnificentmrb@gmail.com>
2017-06-20 14:05:16 -0700

    Joysticks: Remove holdtime parameter

--- xbmc/input/joysticks/DefaultJoystick.cpp
+++ xbmc/input/joysticks/DefaultJoystick.cpp
@@ -72,11 +72,6 @@ bool CDefaultJoystick::AcceptsInput()
   return g_application.IsAppFocused();
 }
 
-unsigned int CDefaultJoystick::GetDelayMs(const FeatureName& feature) const
-{
-  return m_handler->GetHoldTimeMs(GetKeyID(feature), GetWindowID(), GetFallthrough());
-}
-
 bool CDefaultJoystick::OnButtonPress(const FeatureName& feature, bool bPressed)
 {
   if (bPressed && m_easterEgg && m_easterEgg->OnButtonPress(feature))
